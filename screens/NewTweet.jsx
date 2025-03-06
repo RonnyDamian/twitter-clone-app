b3005db23226be9a06ae4d6bce120e17f07b6396
Ronny Rodriguez <rrodriguez@tsa.ec>
2025-03-06 15:35:09 -0500

    Initial commit

---
+++ screens/NewTweet.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+export default function NewTweet() {  
+  return (
+    <View style={{flex:1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text> New Tweet </Text>
+    </View>
+  );  
+}
